5d4691533e896c240f0ce9e9887eea98ae593aa6
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-02-29 01:51:08 +0530

    add: score & bestScore state logic

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import Card from './Card';
 
 function Cards() {
   const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const [consecSelect, setConsecSelect] = useState([]);
+
+  // Score State
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  // Updating best score
+  if (score > bestScore) setBestScore(score);
+
+  // Update score
+  const handleClick = (e) => {
+    const value = e.target.children.length
+      ? e.target.children[0].textContent
+      : e.target.textContent;
+    if (consecSelect.includes(value)) {
+      setScore(0);
+      setConsecSelect([]);
+    } else {
+      setScore(score + 1);
+      setConsecSelect([...consecSelect, value]);
+    }
+  };
 
   return (
     <>
+      <div className='score'>
+        Score:{score} Best score:{bestScore}
+      </div>
       <div className='cards'>
         {data.map((i) => {
           return <Card key={i} name={i} />;
