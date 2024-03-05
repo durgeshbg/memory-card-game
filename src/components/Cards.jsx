59b2dca4b96090ebcec8dae46ada2fc1b66d4b95
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-05 23:50:53 +0530

    feat: level selection

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import Card from './Card';
 import '../styles/Cards.css';
 import Loading from './Loading';
+import Level from './Level';
 
 function Cards() {
   const [data, setData] = useState({});
   const [consecSelect, setConsecSelect] = useState([]);
+  const [level, setLevel] = useState(0);
+  const n = 3 * (level + 2);
 
   // Score State
   const [score, setScore] = useState(0);
@@ -13,14 +16,14 @@ function Cards() {
 
   // Fetch images
   useEffect(() => {
-    if (Object.keys(data).length >= 12) return;
+    if (Object.keys(data).length >= n) return;
     const i = Math.floor(Math.random() * 1000);
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       .then((raw) => raw.json())
       .then((res) => {
         setData({ ...data, [res.name]: res.sprites.front_default });
       });
-  }, [data]);
+  }, [data, level]);
 
   // Update score
   const handleClick = (e) => {
@@ -44,15 +47,19 @@ function Cards() {
         <div className='score'>
           Score:{score} Best score:{bestScore}
         </div>
-        <div className='cards'>
-          {Object.keys(temp).length < 12 ? (
-            <Loading />
-          ) : (
-            Object.keys(temp).map((name) => (
-              <Card handleClick={handleClick} url={data[name]} key={name} name={name} />
-            ))
-          )}
-        </div>
+        {!level ? (
+          <Level setLevel={setLevel} />
+        ) : (
+          <div className='cards'>
+            {Object.keys(temp).length < n ? (
+              <Loading />
+            ) : (
+              Object.keys(temp).map((name) => (
+                <Card handleClick={handleClick} url={data[name]} key={name} name={name} />
+              ))
+            )}
+          </div>
+        )}
       </main>
     </>
   );
